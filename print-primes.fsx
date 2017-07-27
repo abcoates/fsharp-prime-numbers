8a1a954f1633e282d2eb5fbc56a0723c57ff1539
Anthony Coates <abcoates@contakt.org>
2017-07-27 19:57:24 +1000

    Created an improved (faster) "print-primes" F# script.

--- print-primes.fsx
+++ print-primes.fsx
@@ -2,6 +2,10 @@
 open org.contakt.math.prime.PrimeNumbers
 open System
 let sw = System.Diagnostics.Stopwatch()
-let mutable n: uint32 = 0u
+let rec printEvery (n: int) (index: int) (s: int seq) =
+    let newindex = index + n
+    printfn "%A" (newindex, s |> Seq.take n |> Seq.last, (sw.ElapsedMilliseconds + 500L) / 1000L)
+    printEvery n newindex (s |> Seq.skip n)
+printfn "(#, prime, sec)"
 sw.Start()
-n <- 0u; primes |> Seq.iter (fun i -> n <- n + 1u; if (n % 1000u = 0u) then printfn "prime #%d = %d in %d seconds" n i ((sw.ElapsedMilliseconds + 500L) / 1000L))
+printEvery 1000 0 primes
